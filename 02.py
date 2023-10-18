b9803a76b2c4c2b9d139c15576c555350b6fc8f4
Kettengeschwindigkeit <666damned999@mail.ru>
2023-10-18 23:01:47 +0300

    variables, output, data types

---
+++ 02.py
@@ -0,0 +1,53 @@
+# data output
+
+print (444)
+print ('Hello')
+print (44, 33, 'Hi')
+print (5, '6')
+
+# variables
+
+foo = 6666
+a = 5
+helloPython = 9999
+bar = 'Python'
+
+print (foo, a)
+print (a)
+print (foo + helloPython)
+
+a = 11
+b = 12
+
+print (a)
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+
+c = a + b
+d = a - b
+
+print (c)
+print (d)
+
+# float
+
+f = 5.5
+g = 0.5
+h = .5
+
+print (f, g, h)
+
+# data types
+
+print (type (f))  
+print (type (b))
+print (type (10 / 5))
+
+k = True
+K = False
+
+print (k)
+print (K)
+print (type (k))
